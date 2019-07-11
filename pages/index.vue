9d10c1742ba35d92c876fb6cc22f44cba47cb6a9
Jason Ma <jason@madebyon.com>
2019-07-12 00:13:54 +0800

    Opening screen

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,15 @@
 <template>
   <section class="main">
     <div class="building-01">
-      <div class="inner"><div v-for="(n,i) in 80" class="window" :class="{'light': (n==17 || n==47)}"></div></div>
+      <div class="inner">
+        <div v-for="(n,i) in 88" class="window" :class="{'light': (n==17 || n==47 || n==62)}"></div>
+      </div>
+    </div>
+    <div class="building-02">
+      <div class="inner">
+        <div class="ceiling"></div>
+        <div class="wall"><div class="inner"><div v-for="(n,i) in 80" class="window" :style="{'left': i*33 + 'px'}"></div></div></div>
+      </div>
     </div>
   </section>
 </template>
@@ -16,18 +24,20 @@
 
 <style lang="scss">
   .main {
-    background-color: #0c3545;
+    background-color: #a0e2fd;
     height: 100vh;
     position: relative;
+    overflow: hidden;
   }
 
   .building-01 {
-    width: 200px;
-    height: 420px;
+    width: 180px;
+    height: 440px;
     background-color: #598d98;
     position: absolute;
     top: 60px;
-    left: 20px;
+    left: 50%;
+    transform: translate(-240px, 0px);
 
     .inner {
       position: relative;
@@ -50,18 +60,18 @@
         left: 0;
         width: 0;
         border-style: solid;
-        border-width: 200px 200px 0 0;
+        border-width: 200px 180px 0 0;
         border-color: #566d7b transparent transparent transparent;
         z-index: 0;
       }
 
       .window {
-        width: 12px;
+        width: 10px;
         height: 22px;
         background-color: #99aab2;
         float: left;
         position: relative;
-        margin: 6px 5px;
+        margin: 6px 5px 2px;
         z-index: 2;
         position: relative;
 
@@ -76,6 +86,46 @@
 
         &.light {
           background-color: #e1ba7f;
+        }
+      }
+    }
+  }
+
+  .building-02 {
+    width: 100%;
+    height: 220px;
+    position: absolute;
+    top: 420px;
+    left: 0;
+
+    .inner {
+      position: relative;
+
+      .ceiling {
+        position: absolute;
+        top: 0;
+        width: 100%;
+        height: 20px;
+        background-color: #ccc;
+      }
+
+      .wall {
+        position: absolute;
+        top: 50px;
+        width: 100%;
+        height: 50px;
+        background-color: #ccc;
+
+        .inner {
+          position: relative;
+
+          .window {
+            position: absolute;
+            top: 0;
+            width: 30px;
+            height: 15px;
+            background-color: #aaa;
+          }
         }
       }
     }
